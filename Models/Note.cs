719dd9b1f7505fc4e26918ce63c46e150a200888
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2023-06-11 12:30:06 +0300

    Add files via upload

---
+++ Models/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace NoteHub.Models
+{
+    public class Note
+    {
+        public string UserId { get; set; }
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteDescription { get; set; }
+
+        public Note()
+        {
+
+        }
+    }
+}
